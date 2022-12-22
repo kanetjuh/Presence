4b1845e29802cf6e8bd36f34415abdbd45ea3bc1
Connor <halil_ismail@yahoo.com>
2022-12-22 22:44:13 +0100

    feat: added timestamps, a small margin at the bottom, and contributor notes

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,6 +1,6 @@
-import { FormInput, FormSection, KeyboardAvoidingView, ScrollView } from 'enmity/components';
+import { Button, FormInput, FormRow, FormSection, FormSwitch, KeyboardAvoidingView, ScrollView, Text, View } from 'enmity/components';
 import { SettingsStore } from 'enmity/api/settings';
-import { React } from 'enmity/metro/common';
+import { Linking, React } from 'enmity/metro/common';
 import { getActivity } from '../activity';
 import { setActivity } from '../rpc';
 
@@ -12,7 +12,7 @@ export default ({ settings }: SettingsProps) => {
 	const basicData = [
 		{ item: 'Name', placeholder: 'Apeture Science' },
 		{ item: 'Details', placeholder: 'Cooperative Testing Initiative' },
-		{ item: 'State', placeholder: 'Testing²' }
+		{ item: 'State', placeholder: 'Testing²' },
 	]
 	const imageData = [
 		{ image: 'Large', placeholder_url: 'teambuilding_testchamber01', placeholder: 'Team Building Test Chamber 01' },
@@ -25,8 +25,10 @@ export default ({ settings }: SettingsProps) => {
 
 	React.useEffect(() =>
 		() => {
-			if (settings.get('applicationId', null) && settings.get('name', null)) {
+			if (settings.get('enabled')) {
 				console.log("Setting activity on settings close")
+				// Set timestamps on start (in ms)
+				if (settings.get('timestamps')) settings.set('startTimestamp', Date.now())
 				setActivity(getActivity())
 			} else {
 				setActivity(undefined)
@@ -35,6 +37,18 @@ export default ({ settings }: SettingsProps) => {
 
 	return <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
 		<ScrollView>
+		<FormSection title="Enable">
+				<FormRow
+					label="Enable Rich Presence"
+					subLabel="Enable or disable the Rich Presence"
+					trailing={(
+						<FormSwitch
+							value={settings.get('enabled')}
+							onValueChange={(value) => settings.set('enabled', value)}
+						/>
+					)}
+				/>
+                </FormSection>
 			<FormSection title="Basic">
 				<FormInput
 					value={settings.get('applicationId')}
@@ -50,6 +64,30 @@ export default ({ settings }: SettingsProps) => {
 						placeholder={placeholder}
 					/>
 				)}
+			</FormSection>
+			<FormSection title="Timestamps">
+				<FormRow
+					label="Enable timestamps"
+					subLabel="Enable or disable the timestamps"
+					trailing={(
+						<FormSwitch
+							value={settings.get('timestamps')}
+							onValueChange={(value) => settings.set('timestamps', value)}
+						/>
+					)}
+				/>
+				<FormInput
+					value={settings.get('startTimestamp')}
+					onChange={(value) => settings.set('startTimestamp', value || undefined)}
+					title="Start timestamp"
+					placeholder="12345678910 (in ms)"
+				/>
+				<FormInput
+					value={settings.get('endTimestamp')}
+					onChange={(value) => settings.set('endTimestamp', value || undefined)}
+					title="End timestamp"
+					placeholder="12345678910 (in ms)"
+				/>
 			</FormSection>
 			<FormSection title="Images">
 				{imageData.map(({ image, placeholder, placeholder_url }) =>
@@ -87,6 +125,9 @@ export default ({ settings }: SettingsProps) => {
 					</>
 				)}
 			</FormSection>
+			<View style={{ marginBottom: 50 }} />
+			<Text style={{ textAlign: 'center', color: '#fff', fontSize: 12 }}>Developers: <Text style={{ color: '#7289da' }} onPress={() => Linking.openURL('https://fiery.gay/')}>Fiery</Text> and <Text style={{ color: '#7289da' }} onPress={() => Linking.openURL('https://connor.is-a.dev')}>Connor</Text></Text>
+			<View style={{ marginBottom: 50 }} />
 		</ScrollView>
 	</KeyboardAvoidingView>;
 };
